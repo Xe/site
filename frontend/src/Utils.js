cf0cb7b7807df255e6f8ac7c92f8bd650d41e8c0
Christine Dodrill <me@christine.website>
2016-12-14 18:23:07 -0800

    frontend/Utils: better unescape HTML encoded strings

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -1,9 +1,14 @@
 // Module App.BlogEntry
 
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
 exports.mdify = function(id) {
   var converter = new showdown.Converter()
   elem = document.getElementById(id);
   md = elem.innerHTML;
-  elem.innerHTML = unescape(converter.makeHtml(md));
+  elem.innerHTML = htmlDecode(converter.makeHtml(md));
   return "done :)";
 }
