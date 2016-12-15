7c4d5314934c069ac6ec435bcc8582ed2b50b708
Christine Dodrill <me@christine.website>
2016-12-14 21:16:44 -0800

    frontend/Utils: oh god undo this

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -1,13 +1,9 @@
 // Module App.BlogEntry
 
-function htmlDecode(value){
-  return $('<div/>').html(value).text();
-}
-
 exports.mdify = function(id) {
   var converter = new showdown.Converter()
   elem = document.getElementById(id);
   md = elem.innerHTML;
-  elem.innerHTML = htmlDecode(converter.makeHtml(md));
+  elem.innerHTML = converter.makeHtml(md);
   return "done :)";
 }
