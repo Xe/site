fa9ad2f1c875199c55832d69cdc59166a24fb891
Christine Dodrill <me@christine.website>
2016-12-14 18:15:33 -0800

    frontend/Utils: unescape compiled html output

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -4,6 +4,6 @@ exports.mdify = function(id) {
   var converter = new showdown.Converter()
   elem = document.getElementById(id);
   md = elem.innerHTML;
-  elem.innerHTML = converter.makeHtml(md);
+  elem.innerHTML = unescape(converter.makeHtml(md));
   return "done :)";
 }
