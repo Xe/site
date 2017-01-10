8fe2cfd62306449cc1962a36b7f4462b3de6de0a
Christine Dodrill <me@christine.website>
2017-01-10 12:39:15 -0800

    build.sh: be more sane

--- build.sh
+++ build.sh
@@ -6,14 +6,6 @@ set -x
 export PATH="$PATH:/usr/local/go/bin"
 export CI="true"
 
-(cd /site/backend/christine.website
- go get github.com/Xe/asarfs
- go get github.com/gernest/front
- go get layeh.com/asar
- go get gopkg.in/yaml.v2
- go get github.com/urfave/negroni
-
- go build -v
- mv christine.website /usr/bin) &
-
-wait
+cd /site/backend/christine.website
+go build -v
+mv christine.website /usr/bin
