9cbb20aea2d6b1979a47af9956dbcc8dbe2a2e08
Christine Dodrill <me@christine.website>
2017-05-20 15:43:42 -0700

    add vendor dependencies

---
+++ vendor/github.com/Xe/gopreload/doc.go
@@ -0,0 +1,7 @@
+/*
+Package gopreload is a bit of a hack to emulate the behavior of LD_PRELOAD [ld-preload].
+This allows you to have automatically starting instrumentation, etc.
+
+[ld-preload]: http://man7.org/linux/man-pages/man8/ld.so.8.html (see LD_PRELOAD section)
+*/
+package gopreload
