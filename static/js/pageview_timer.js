e4768b407305d5e2b3b808cf6d75f8a1a2426f55
Christine Dodrill <me@christine.website>
2019-08-19 16:43:29 +0000

    experiment: track pageview times

---
+++ static/js/pageview_timer.js
@@ -0,0 +1,30 @@
+/*
+  Hi,
+
+  If you are reading this, you have found this script in the referenced scripts
+  for pages on this site. I know you're gonna have to take me at my word on this,
+  but I'm literally using this to collect how much time people spend reading my
+  webpages. See metrics here: https://christine.website/metrics
+
+  If you have the "do not track" setting enabled in your browser, this code will
+  be ineffectual.
+*/
+
+let startTime = new Date();
+
+function logTime() {
+    let stopTime = new Date();
+    let message = JSON.stringify(
+        {
+            "path": window.location.pathname,
+            "start_time": startTime.toISOString(),
+            "end_time": stopTime.toISOString(),
+        }
+    );
+
+    if (!window.navigator.sendBeacon("/api/pageview-timer", message)) {
+        alert("wtf");
+    }
+}
+
+window.addEventListener("pagehide", logTime, false);
