fa0b111b63487f8df9cb8d417fa0e47064042377
Christine Dodrill <me@christine.website>
2020-02-10 19:17:59 -0500

    blog: I was Wrong About Nix (#116)

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
   pkgs = import <nixpkgs> { };
   sources = import ./nix/sources.nix;
-  vgo2nix = (import sources.vgo2nix { });
-in pkgs.mkShell { buildInputs = [ pkgs.go pkgs.niv vgo2nix ]; }
+  xepkgs = import sources.xepkgs { };
+  vgo2nix = import sources.vgo2nix { };
+in pkgs.mkShell { buildInputs = [ pkgs.go pkgs.niv xepkgs.gopls vgo2nix ]; }
