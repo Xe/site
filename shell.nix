d2728952c7d8d325f3dae646a779d9b7f89d6087
Christine Dodrill <me@christine.website>
2020-02-12 14:18:59 +0000

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  pkgs = import <nixpkgs> { };
   sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
   xepkgs = import sources.xepkgs { };
   vgo2nix = import sources.vgo2nix { };
 in pkgs.mkShell { buildInputs = [ pkgs.go pkgs.niv xepkgs.gopls vgo2nix ]; }
