56de8e261ee81f6c35671c0baaed22c4b38edd5d
Christine Dodrill <me@christine.website>
2020-07-16 10:53:03 -0400

    clean up deploy

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -p doctl -p kubectl -i bash
+#! nix-shell -p doctl -p kubectl -p curl -i bash
 nix-env -if ./nix/dhall-yaml.nix
 doctl kubernetes cluster kubeconfig save kubermemes
 dhall-to-yaml-ng < ./site.dhall | kubectl apply -n apps -f -
 kubectl rollout status -n apps deployment/christinewebsite
+kubectl apply -f ./k8s/job.yml
+sleep 10
+kubectl delete -f ./k8s/job.yml
+curl -H "Authorization: $MI_TOKEN" --data "https://christine.website/blog.json" https://mi.within.website/blog/refresh
