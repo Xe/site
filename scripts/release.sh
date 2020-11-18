f8ae558738d1b2639a68661b6d516a7844baab57
Christine Dodrill <me@christine.website>
2020-11-18 13:15:33 -0500

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -p doctl -p kubectl -p curl -i bash
+#! nix-shell -I nixpkgs=https://nixos.org/channels/nixpkgs-unstable
+
 nix-env -if ./nix/dhall-yaml.nix
 doctl kubernetes cluster kubeconfig save kubermemes
 dhall-to-yaml-ng < ./site.dhall | kubectl apply -n apps -f -
