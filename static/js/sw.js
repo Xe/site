7c3a00ff1137f5576d87ce10daccfabeb7120781
Xe <me@christine.website>
2021-12-21 13:19:58 -0500

    turn up the fuck out of the contrast
    Signed-off-by: Xe <me@christine.website>

--- static/js/sw.js
+++ static/js/sw.js
@@ -5,12 +5,10 @@ self.addEventListener('install', function (event) {
   event.waitUntil(preLoad());
 });
 
-const cacheName = "cache-xesite-2.3.0-999";
+const cacheName = "cache-xesite-2.3.0-1000";
 
 var preLoad = function () {
-  console.log('[PWA Builder] Install Event processing');
   return caches.open(cacheName).then(function (cache) {
-    console.log('[PWA Builder] Cached index and offline page during Install');
     return cache.addAll(['/blog/', '/blog', '/', '/contact', '/resume', '/talks', '/gallery', '/signalboost']);
   });
 };
@@ -19,7 +17,6 @@ self.addEventListener('fetch', function (event) {
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
     return;
   }
-  console.log('[PWA Builder] The service worker is serving the asset.');
   event.respondWith(checkResponse(event.request).catch(function () {
     return returnFromCache(event.request);
   }));
@@ -41,7 +38,6 @@ var checkResponse = function (request) {
 var addToCache = function (request) {
   return caches.open(cacheName).then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('[PWA Builder] add page to offline: ' + response.url);
       return cache.put(request, response);
     });
   });
