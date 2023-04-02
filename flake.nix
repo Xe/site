2cc14d7e10b29decf3ef1e88550ec603a78322e2
Xe Iaso <me@xeiaso.net>
2023-04-02 15:00:55 -0400

    fix iosevka for maple syrup in french
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
                     --layout-features=* \
                     --no-hinting \
                     --desubroutinize \
-                    --unicodes="U+0000-00A0,U+00A2-00A9,U+00AC-00AE,U+00B0-00B7,U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD"
+                    --unicodes="U+0000-0170,U+00D7,U+00F7,U+2000-206F,U+2074,U+20AC,U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,U+FFFD,U+00E8"
               done
 
             '';
