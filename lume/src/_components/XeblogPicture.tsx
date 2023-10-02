7939a6deaef4fba9d70417005d7931f41855ce5a
Xe Iaso <me@xeiaso.net>
2023-10-02 13:55:39 -0400

    blog: gokrazy SD card resizing post
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- lume/src/_components/XeblogPicture.tsx
+++ lume/src/_components/XeblogPicture.tsx
@@ -5,22 +5,24 @@ export interface XeblogPicture {
 
 export default function XeblogPicture({ path, desc }: XeblogPicture) {
   return (
-    <figure>
-      <picture>
-        <source
-          type="image/avif"
-          srcset={`https://cdn.xeiaso.net/file/christine-static/${path}.avif`}
-        />
-        <source
-          type="image/webp"
-          srcset={`https://cdn.xeiaso.net/file/christine-static/${path}.webp`}
-        />
-        <img
-          alt={`An image of ${prompt}`}
-          loading="lazy"
-          src={`https://cdn.xeiaso.net/file/christine-static/${path}.jpg`}
-        />
-      </picture>
+    <figure className="max-w-3xl mx-auto">
+      <a href={`https://cdn.xeiaso.net/file/christine-static/${path}.jpg`}>
+        <picture>
+          <source
+            type="image/avif"
+            srcset={`https://cdn.xeiaso.net/file/christine-static/${path}.avif`}
+          />
+          <source
+            type="image/webp"
+            srcset={`https://cdn.xeiaso.net/file/christine-static/${path}.webp`}
+          />
+          <img
+            alt={desc}
+            loading="lazy"
+            src={`https://cdn.xeiaso.net/file/christine-static/${path}.jpg`}
+          />
+        </picture>
+      </a>
       {desc && <figcaption>{desc}</figcaption>}
     </figure>
   );
