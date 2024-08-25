4d598314c8d17102d3a0fdc5a89eb531a2f6898f
Xe Iaso <me@xeiaso.net>
2024-08-25 14:48:24 -0400

    xecast: lol oops fuck you need to link to RSS
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- lume/src/xecast.jsx
+++ lume/src/xecast.jsx
@@ -8,6 +8,8 @@ export default ({ search }, { date }) => {
 
       <p className="mb-4">Welcome to Xecast! Here you can find all the episodes of the podcast. Xecast is an experimental podcast where I talk about computing, AI, technology, and other topics that I find interesting.</p>
 
+      <p className="mb-4"><a href="/xecast.rss">Subscribe via RSS 📡</a></p>
+
       <ul class="list-disc ml-4 mb-4">
         {search.pages("is_xecast=true", "order date=desc")
           .filter(post => post.index)
