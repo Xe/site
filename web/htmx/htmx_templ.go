3b9b68236aa52074bd4ea14251ffaf5727aef03b
Xe Iaso <me@xeiaso.net>
2026-02-07 17:07:50 +0000

    chore(web/htmx): go generate again
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- web/htmx/htmx_templ.go
+++ web/htmx/htmx_templ.go
@@ -5,10 +5,8 @@ package htmx
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
 // Use pulls in the HTMX core library and any extensions.
 //
