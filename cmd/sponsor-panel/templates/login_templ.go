ba382cda86bf8e6c5657346be834731be1a067f2
Xe Iaso <me@xeiaso.net>
2026-02-07 19:42:11 +0000

    fix(sponsor-panel/templates): remove dead argument
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- cmd/sponsor-panel/templates/login_templ.go
+++ cmd/sponsor-panel/templates/login_templ.go
@@ -8,7 +8,7 @@ package templates
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Login(discordInvite string) templ.Component {
+func Login() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
