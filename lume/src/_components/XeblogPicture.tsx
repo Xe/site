ffa1fbf66bb6f0c9335c67b29b772b1ac501edcf
Xe Iaso <me@xeiaso.net>
2026-09-20 12:22:06 -0400

    feat(blog): add This is why we play
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- lume/src/_components/XeblogPicture.tsx
+++ lume/src/_components/XeblogPicture.tsx
@@ -2,15 +2,21 @@ export interface XeblogPicture {
   path: string;
   desc?: string;
   className?: string;
+  fullBleed?: boolean;
 }
 
 export default function XeblogPicture({
   path,
   desc,
   className,
+  fullBleed,
 }: XeblogPicture) {
+  const figClass = fullBleed
+    ? `full-bleed not-prose my-8 ${className ?? ""}`
+    : `max-w-3xl mx-auto not-prose w-full ${className ?? ""}`;
+
   return (
-    <figure className={`max-w-3xl mx-auto not-prose w-full ${className}`}>
+    <figure className={figClass}>
       <a href={`https://files.xeiaso.net/${path}.jpg`}>
         <picture>
           <source
@@ -29,7 +35,7 @@ export default function XeblogPicture({
           />
         </picture>
       </a>
-      {desc && <figcaption>{desc}</figcaption>}
+      {desc && <figcaption className="max-w-xl">{desc}</figcaption>}
     </figure>
   );
 }
